fa280d2798d51468a76180db1c2c5470778ea0ce
rubal18csu180 <rubal18csu180@ncuindia.edu>
2019-01-29 22:25:37 +0530

    done

---
+++ interface/src/Q6/resizable_main.java
@@ -0,0 +1,10 @@
+class resizable_main
+{
+	public static void main(String args[])
+	{
+      ResizableCircle rc=new ResizableCircle(10.0);
+      System.out.println("perimeter "+rc.getPerimeter());
+      System.out.println("area "+rc.getArea());
+      
+	}
+}
